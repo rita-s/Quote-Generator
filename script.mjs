47ee12cd4a3dcd1d9997dbcb78299daf2cb12ceb
rita-s <rita_s@mail.com>
2024-03-07 23:53:17 +0000

    adding event listeners

--- script.mjs
+++ script.mjs
@@ -1,11 +1,66 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const xButton = document.getElementById('X');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Local Quotes
 // Show New Quote
 function newQuote(){
     // Pick a random quote from localQuotes array
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote);
+
+    // Check Quote length to determine styling
+    if (quote.text.length > 120){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+
+    //Replace Author with 'Unknown' if Author field is blank
+    if (!quote.author){
+        authorText.textContent = 'Unknown';
+    }else{
+        authorText.textContent = quote.author;
+    }
 }
 newQuote();
+
+
+// Tweet / X Quote
+function xQuote(){
+    const xUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(xUrl, '_blank')
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+xButton.addEventListener('click', xQuote);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
